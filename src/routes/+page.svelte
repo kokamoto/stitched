df78175a8d7b03d601f12cd40f8a7d413f3fcbbe
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2024-01-28 10:45:19 -0800

    Adding Menu component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Menu from '$lib/Menu.svelte';
+
+  let fill = "red";
+</script>
+
+<h1>Welcome to Stitched</h1>
+
+<Menu/>
+
+<label for="fill-color">Fill color:</label>
+<select bind:value={fill} id="fill-color">
+  <option value="blue">blue</option>
+  <option value="red">red</option>
+  <option value="green">green</option>
+</select>
+
+<br/>
+
+<svg width="300" height="100">
+  <title>Attribute Animation with SMIL</title>
+  <rect x="0" y="0" width="300" height="100" stroke="black" stroke-width="1" />
+  <circle cx="0" cy="50" r="15" fill="{fill}" stroke="black" stroke-width="1">
+    <title>A {fill} circle</title>
+    <animate
+      attributeName="cx"
+      from="0"
+      to="500"
+      dur="8s"
+      repeatCount="indefinite" />
+  </circle>
+</svg>
---
+++ src/routes/lab/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Menu from "$lib/Menu.svelte";
+</script>
+<h1>The Lab</h1>
+
+<Menu/>
