38ab0e62186a33aeb255661e8639805ef7118585
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2024-02-24 10:28:32 -0800

    Adding initial page/route for Terminal Interface

--- src/routes/lab/+page.svelte
+++ src/routes/lab/+page.svelte
@@ -1,1 +1,6 @@
 <h1>The Lab</h1>
+
+<ul>
+  <li><a href="/lab/experiments-with-svg">Experiments with SVG</a></li>
+  <li><a href="/lab/terminal-interface">Terminal Interface</a></li>
+</ul>
---
+++ src/routes/lab/terminal-interface/+page.svelte
@@ -0,0 +1,10 @@
+<style>
+  .terminal {
+    font-family: monospace;
+  }
+</style>
+<h1>Lab: Terminal Interface</h1>
+
+<div class="terminal">
+> This is a terminal!
+</div>
