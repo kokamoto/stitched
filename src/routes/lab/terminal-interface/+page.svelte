69108ca27b59f8c75d9b7b88aa408d40ae41dd63
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2024-02-29 19:07:21 -0800

    Working on terminal styles

--- src/routes/lab/terminal-interface/+page.svelte
+++ src/routes/lab/terminal-interface/+page.svelte
@@ -2,9 +2,44 @@
   .terminal {
     font-family: monospace;
   }
+  .terminal div {
+    line-height: 1.5rem;
+  }
+  .terminal .prompt {
+    opacity: 0.7;
+  }
+  .prompt.blink {
+    animation: blink-animation 1s steps(5, start) infinite;
+    -webkit-animation: blink-animation 1s steps(5, start) infinite;
+  }
+  @keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+  @-webkit-keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
 </style>
 <h1>Lab: Terminal Interface</h1>
 
 <div class="terminal">
-> This is a terminal!
+  <div class="terminal-output">
+    <span>This is a terminal!</span>
+  </div>
+  <div class="terminal-command">
+    <span>foobar</span>
+  </div>
+  <div class="terminal-error">
+    <span>'foobar' not a valid command</span>
+  </div>
+  <div class="terminal-command">
+    <span>help</span>
+  </div>
+  <div class="terminal-input">
+    <span></span>
+    <span class="prompt blink">&#9608;</span>
+  </div>
 </div>
