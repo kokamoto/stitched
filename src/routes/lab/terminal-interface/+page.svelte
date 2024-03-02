fb96e583d2e96c7938b77c6de919d903e8c91721
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2024-03-01 19:44:28 -0800

    Adding command input to terminal example

--- src/routes/lab/terminal-interface/+page.svelte
+++ src/routes/lab/terminal-interface/+page.svelte
@@ -1,28 +1,43 @@
 <style>
   .terminal {
     font-family: monospace;
+    border: 1px solid #333;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    cursor: text;
   }
   .terminal div {
     line-height: 1.5rem;
   }
-  .terminal .prompt {
-    opacity: 0.7;
-  }
-  .prompt.blink {
-    animation: blink-animation 1s steps(5, start) infinite;
-    -webkit-animation: blink-animation 1s steps(5, start) infinite;
-  }
-  @keyframes blink-animation {
-    to {
-      visibility: hidden;
-    }
-  }
-  @-webkit-keyframes blink-animation {
-    to {
-      visibility: hidden;
-    }
+  .terminal-input input {
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    border: none;
+    outline: none;
   }
 </style>
+
+<script lang="ts">
+  let cmd = "";
+
+  let commands: string[] = [
+    "This is a terminal",
+    "Foobar",
+    "help"
+  ]
+
+  function onCmdEnter(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      if (event.target instanceof HTMLInputElement) {
+        commands = [...commands, event.target.value]
+        cmd = "";
+      }
+    }
+  }
+
+</script>
+
 <h1>Lab: Terminal Interface</h1>
 
 <div class="terminal">
@@ -35,11 +50,12 @@
   <div class="terminal-error">
     <span>'foobar' not a valid command</span>
   </div>
+  {#each commands as command}
   <div class="terminal-command">
-    <span>help</span>
+    <span>{command}</span>
   </div>
+  {/each}
   <div class="terminal-input">
-    <span></span>
-    <span class="prompt blink">&#9608;</span>
+    <input bind:value={cmd} on:keydown={onCmdEnter}/>
   </div>
 </div>
